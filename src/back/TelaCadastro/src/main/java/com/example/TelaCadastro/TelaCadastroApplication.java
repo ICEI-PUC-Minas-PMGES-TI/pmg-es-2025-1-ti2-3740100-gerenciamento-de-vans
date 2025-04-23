30d302a15646820490304028079d5f196942c188
Joao K Oliveira <165311816+JoaoKOliv@users.noreply.github.com>
2025-04-23 20:17:13 -0300

    Configuração TelaCadastro

---
+++ src/back/TelaCadastro/src/main/java/com/example/TelaCadastro/TelaCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.example.TelaCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelaCadastroApplication.class, args);
+	}
+
+}
